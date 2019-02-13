33bf0be3cce82dc40c938cfd31c4da247b97c9eb
Eric Moyer <enm.ridercoach@gmail.com>
2019-02-13 07:39:22 -0500

    Moving along with 2-D PCA Playground.

--- app.R
+++ app.R
@@ -20,7 +20,7 @@ get_pca_df <- function(df) {
   pca <- prcomp(df)
   m <- t(pca$rotation)
   m_wts <- matrix(rep(summary(pca)$importance[1,], 2), nrow=2)
-  df_pca <- data.frame("PC" = rownames(m), as.data.frame(m * m_wts))
+  df_pca <- data.frame("PC" = rownames(m), as.data.frame(m * m_wts * 2))
 }
 
 ui <- fluidPage(
@@ -42,23 +42,23 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+  
    output$distPlot <- renderPlot({
-
+     
       x_coords <- get_coords(input$sdx)     
       y_coords <- get_coords(input$sdy)
       lim <- get_axes_limit(x_coords, y_coords)
-      
+     
       df <- data.frame("x" = x_coords, "y" = y_coords)
       df_pca <- get_pca_df(df)
-    
+     
       ggplot(df) + 
         geom_point(aes(x, y), color = "blue", alpha = 0.3) + 
         geom_segment(data = df_pca, 
                      aes(x = 0, y = 0, xend = x, yend = y, color = PC), 
                      size = 2) + 
         xlim(-lim, lim) + ylim(-lim, lim)
-   }, height = 800, width = 800)
+   }, height = 600, width = 600)
 }
 
 # Run the application
